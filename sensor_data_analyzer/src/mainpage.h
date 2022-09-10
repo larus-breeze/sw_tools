7508517d8fa6f044bf600b0576f06fcbbf37bd60
realtimepeople <klaus.schaefer@h-da.de>
2022-09-11 00:25:51 +0200

    Slight mod.

--- sensor_data_analyzer/src/mainpage.h
+++ sensor_data_analyzer/src/mainpage.h
@@ -4,8 +4,10 @@
  * and feeds the sensor readings through the original flight processing algorithms.
  *
  * The output is either a binary data file with all derived properties
- * or information going to a XCsoar instance in real-time via TCP and CAN bus data tunneled
- * through a USB->USART dongle to a flight display.
+ * or information going to a XCsoar instance in real-time via TCP.
  *
+ * Additionally CAN bus data in the Larus format can be tunneled
+ * through a USB->USART dongle to an AD57 flight display.
+
  * The complete project is hosted at https://github.com/larus-breeze
 */
