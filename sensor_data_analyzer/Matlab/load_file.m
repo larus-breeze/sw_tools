4581e647ecc3f77901535f7431b21ffbb8e34627
realtimepeople <klaus.schaefer@h-da.de>
2022-09-20 19:37:34 +0200

    Removed MATLAB

--- sensor_data_analyzer/Matlab/load_file.m
+++
@@ -1,6 +0,0 @@
-clear;
-f=fopen(['/home/schaefer.open/Flight_Logs/220725_OM_Data/20220724_STGT.f50.f112'],'r');
-x=fread(f,[112,inf],'float32');
-fclose(f);
-
-time=linspace(0,length(x)/100/60,length(x));
