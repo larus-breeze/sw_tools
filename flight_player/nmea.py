b808bac781a6886f7a0db08c9ef3e38d8c714a0d
Winfried Simon <winfried.simon@gmail.com>
2023-10-23 15:23:23 +0200

    Smaller and nicer

--- flight_player/nmea.py
+++ flight_player/nmea.py
@@ -2,30 +2,6 @@ import socket, datetime, math
 
 from flight_data import FlightData
 
-def create(*args):
-    """Create a nmea string from given args"""
-    result = ','.join(args)
-    cs = 0
-    for c in result[1:]:
-        cs ^= ord(c)
-    return bytes(f"{result}*{cs:02X}\r\n", "ascii")
-
-def lat_to_str(coord: float):
-    """Convert float coordinate into NMEA ascii string (Latitude)"""
-    if coord>0:
-        coord = coord
-        sign = 'N'
-    else:
-        coord *= -1.0
-        sign = 'S'
-
-    fract, deg = math.modf(coord)
-    return f"{deg:0.0f}{fract*60.0:08.5f},{sign}"
-
-def lon_to_str(coord: float):
-    """Convert float coordinate into NMEA ascii string (Longitude)"""
-    return lat_to_str(coord).replace('N', 'E').replace('S', 'W')
-
 # This class defines some NMEA Strings
 
 class Nmea():
@@ -45,8 +21,8 @@ class Nmea():
         self.send(
                 "$GPGGA",
                 f"{data.time():%H%M%S.%f}"[:-4],
-                lat_to_str(data['Lat']),
-                lon_to_str(data['Long']),
+                Nmea.lat_to_str(data['Lat']),
+                Nmea.lon_to_str(data['Long']),
                 f"{data['sat fix type']:01.0f}",
                 f"{data['sat number']:02.0f}",
                 "1.0",
@@ -64,8 +40,8 @@ class Nmea():
                 "$GPRMC",
                 f"{data.time():%H%M%S.%f}"[:-4],
                 "A",
-                lat_to_str(data['Lat']),
-                lon_to_str(data['Long']),
+                Nmea.lat_to_str(data['Lat']),
+                Nmea.lon_to_str(data['Long']),
                 f"{data['speed GNSS']:05.1f}",
                 f"{data['track GNSS']:05.1f}",
                 f"{data.date_of_flight():%d%m%y}",
@@ -83,3 +59,22 @@ class Nmea():
         nmea_data =  bytes(f"{composed}*{cs:02X}\r\n", "ascii")
 
         self._socket.sendto(nmea_data, (self._ip, self._port))
+
+    @staticmethod
+    def lat_to_str(coord: float):
+        """Convert float coordinate into NMEA ascii string (Latitude)"""
+        if coord>0:
+            coord = coord
+            sign = 'N'
+        else:
+            coord *= -1.0
+            sign = 'S'
+
+        fract, deg = math.modf(coord)
+        return f"{deg:0.0f}{fract*60.0:08.5f},{sign}"
+
+    @staticmethod
+    def lon_to_str(coord: float):
+        """Convert float coordinate into NMEA ascii string (Longitude)"""
+        return Nmea.lat_to_str(coord).replace('N', 'E').replace('S', 'W')
+
