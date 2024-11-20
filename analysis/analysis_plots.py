d82c1b9b20186aa411f2469ff20f3c84170d403e
Maximilian Betz <info.max@e.mail.de>
2024-11-20 18:01:57 +0100

    new concept based on QT started

--- analysis/analysis_plots.py
+++ analysis/analysis_plots.py
@@ -1,27 +1,98 @@
 #!/user/bin/env python3
-import os
-import plotly
-from tkinter import filedialog
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+from process_to_df import ProcessLarus2Df
+from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton, QFileDialog, QRadioButton
+from PyQt6.QtCore import pyqtSlot
+from PyQt6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget
 
-file_path = filedialog.askopenfilename(title="Select a File", filetypes=[("Larus Binary files", "*.f37")])
-eeprom_file_path = str(file_path).replace('.f37', '.EEPROM')
+def plot_mag(df):
+    t = (df.index / 100.0 / 60.0).to_numpy()  # 100Hz ticks to minutes for the time axis
+    print(df.columns)
 
-if not os.path.isfile(eeprom_file_path):
-    raise Exception("There must be a {} file".format(eeprom_file_path))
+    nav_ind_abs = np.sqrt(
+        np.square(df['nav ind mag N']) + np.square(df['nav ind mag E']) + np.square(df['nav ind mag D']))
 
-os.system("{}/data_analyzer_commit_6598331_linux {}".format(os.getcwd(), file_path))
-result_file = str(file_path) + '.f114'
-print('Conversion done.')
-print('Colums: {}'.format())
+    # Plot the data
+    figure, axis = plt.subplots()
+    figure.suptitle("Induction in earth system: ")
+    plt.autoscale(enable=True, axis='y')
+    axis.grid()
+    axis.set_xlabel('t [minutes]')
 
-from plot_mag import plot_mag
+    axis.plot(t, nav_ind_abs.to_numpy(), "k", linewidth=1)
+    axis.legend(["nav ind abs"], loc="lower left")
+    par1 = axis.twinx()
+    par1.plot(t, df['nav ind mag N'].to_numpy(), "r-", linewidth=0.5)
+    par1.plot(t, df['nav ind mag E'].to_numpy(), "g-", linewidth=0.5)
+    par1.plot(t, df['nav ind mag D'].to_numpy(), "b-", linewidth=0.5)
+
+    minimum = df['nav ind mag N'].min()
+    maximum = df['nav ind mag N'].max()
+    if df['nav ind mag E'].min() < minimum:
+        minimum = df['nav ind mag E'].min()
+    if df['nav ind mag E'].max() > maximum:
+        maximum = df['nav ind mag E'].max()
+    if df['nav ind mag D'].min() < minimum:
+        minimum = df['nav ind mag D'].min()
+    if df['nav ind mag D'].max() > maximum:
+        maximum = df['nav ind mag D'].max()
+    par1.set_ylim(minimum - 0.5, maximum + 1.5)
+    par1.legend(['nav ind mag N', 'nav ind mag E', 'nav ind mag D'], loc="lower right")
+    plt.show()
+
+class Window(QWidget):
+    df = None
+    buttonHandle = None
+
+    def __init__(self):
+        super().__init__()
+        fileBtn = QPushButton(text="Open file dialog", parent=self)
+        fileBtn.clicked.connect(self.open_dialog)
+
+        magBtn = QPushButton(text="Plot Mag", parent=self)
+        self.buttonHandle = magBtn
+        magBtn.clicked.connect(self.plot_mag)
+        magBtn.setEnabled(False)
+
+        ahrsBtn = QPushButton(text="Plot AHRS", parent=self)
+        ahrsBtn.setEnabled(False)
 
 
-print('Plotting...')
-plot_mag(result_file)
+        fileBtn.setFixedSize(200, 60)
+        magBtn.setFixedSize(200, 60)
+        ahrsBtn.setFixedSize(200, 60)
+        layout = QVBoxLayout()
+        layout.addWidget(fileBtn)
+        layout.addWidget(magBtn)
+        layout.addWidget(ahrsBtn)
+        self.setLayout(layout)
+
+    @pyqtSlot()
+    def open_dialog(self):
+        file_name = QFileDialog.getOpenFileName(
+            self,
+            "Select a Larus File",
+            "${HOME}",
+            "Larus raw File (*.f37);; Larus Processed File (*.f114)",
+        )
+
+        self.df = ProcessLarus2Df(file_name[0]).get_df()
+        print("Loaded file {} in dataframe".format(file_name[0]))
+        self.buttonHandle.setEnabled(True)
+
+    @pyqtSlot()
+    def plot_mag(self):
+        if self.df is not None:
+            print(self.df)
+        plot_mag(self.df)
 
 
-print('Exiting')
 
-#TODO: create dataframe once and plot two or thee diagrams using plotly.
 
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
--- analysis/process_to_df.py
+++ analysis/process_to_df.py
@@ -2,16 +2,31 @@
 from dataformats import *
 import numpy as np
 import pandas as pd
+import os
+import sys
 
 # Class to import a Larus data logfile into a pandas dataframe.
-class LoadLarus2Df:
+class ProcessLarus2Df:
     df = None
     file = None
-
     def __init__(self, file):
         self.file = file
         if file.endswith('.f37'):
             dataformat = data_f37
+            eeprom_file_path = str(file).replace('.f37', '.EEPROM')
+            result_file = str(file) + '.f114'
+
+            if not os.path.isfile(eeprom_file_path):
+                raise Exception("There must be a {} file".format(eeprom_file_path))
+
+            if not os.path.isfile(result_file):
+                if "linux" in sys.platform:
+                    os.system("{}/data_analyzer_commit_6598331_linux {}".format(os.getcwd(), file))
+                elif "win" in sys.platform:
+                    os.system("{}/data_analyzer_commit_6598331_windows.exe {}".format(os.getcwd(), file))
+            file = result_file
+            dataformat = data_f114
+
         elif file.endswith('.f50'):
             dataformat = data_f50
         elif file.endswith('.f123'):
@@ -31,7 +46,10 @@ class LoadLarus2Df:
         data = np.fromfile(file, dtype=dt, sep="")
         self.df = pd.DataFrame(data)
 
-    def df(self):
+    def get_df(self):
         return self.df
 
 
+if __name__ == "__main__":
+    df = ProcessLarus2Df('240520_091630.f37')
+    print(df.get_df().columns)
