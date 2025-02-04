d05601ce9e5dbd8d6c680e6347060fc338b7a40c
Winfried Simon <winfried.simon@gmail.com>
2025-02-04 17:40:33 +0100

    add Icon for the settings logging window

--- flight_player/flight_player.py
+++ flight_player/flight_player.py
@@ -45,23 +45,24 @@ class Window(QtWidgets.QMainWindow):
         self._exitAction = QtWidgets.QAction(QtGui.QIcon(dir + '/icons/exit.png'), 'Exit', self)
         self._exitAction.triggered.connect(self.close)
         self._exitAction.setStatusTip("Exit Larus Flight Player")
-        self._showLoggerAction = QtWidgets.QAction("Show/Hide Logging Window", self)
-        self._showLoggerAction.setStatusTip("Show the Logging Window")
+        self._showLoggerAction = QtWidgets.QAction(QtGui.QIcon(dir + '/icons/file_log.png'), "Logging Window", self)
         self._showLoggerAction.triggered.connect(self._show_logging)
+        self._showLoggerAction.setStatusTip("Show the Settings Logging Window")
 
         # QMenu
         menu = self.menuBar().addMenu("&Menu")
         menu.addAction(self._openAction)
         menu.addAction(self._playAction)
         menu.addAction(self._pauseAction)
-        menu.addAction(self._exitAction)
         menu.addAction(self._showLoggerAction)
+        menu.addAction(self._exitAction)
 
         # QToolbar
         tools = QtWidgets.QToolBar()
         tools.addAction(self._openAction)
         tools.addAction(self._playAction)
         tools.addAction(self._pauseAction)
+        tools.addAction(self._showLoggerAction)
         tools.addAction(self._exitAction)
         self.addToolBar(tools)
 
