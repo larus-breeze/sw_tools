f1f11e60e86e909ba53896d252453b151b9610a1
realtimepeople <klaus.schaefer@h-da.de>
2025-12-23 11:07:11 +0100

    Lib update.
    File moves project <-> lib

--- sensor_data_analyzer/src/abstract_EEPROM_storage.h
+++
@@ -1,8 +0,0 @@
-#ifndef ABSTRACT_EEPROM_STORAGE_H_
-#define ABSTRACT_EEPROM_STORAGE_H_
-
-#include "persistent_data_file.h"
-
-extern EEPROM_file_system permanent_data_file;
-
-#endif /* ABSTRACT_EEPROM_STORAGE_H_ */
