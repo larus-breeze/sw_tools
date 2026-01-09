071c3003c68ecc2499204185ae0f99c6ded4b080
realtimepeople <klaus.schaefer@h-da.de>
2026-01-09 15:11:19 +0100

    Lib adaptions.

--- sensor_data_analyzer/Interfaces/persistent_data_file_emulation.cpp
+++ sensor_data_analyzer/Interfaces/persistent_data_file_emulation.cpp
@@ -42,10 +42,10 @@ bool read_meta_data_file (char *file_path)
 	}
 
       config_file.seekg (0, ios::beg);
-      config_file.read ((char*) permanent_file, size);
+      config_file.read ((char*) permanent_data_file_storage, size);
       config_file.close ();
 
-      bool result = permanent_data_file.set_memory_area( (uint32_t *)permanent_file, (uint32_t *)(permanent_file+EEPROM_FILE_SYSTEM_SIZE));
+      bool result = permanent_data_file.set_memory_to_existing_data( (uint32_t *)permanent_data_file_storage, (uint32_t *)(permanent_data_file_storage+EEPROM_FILE_SYSTEM_SIZE));
       assert(result == true);
 
       assert( permanent_data_file.is_consistent ());
@@ -95,90 +95,95 @@ bool read_meta_data_file (char *file_path)
       ensure_EEPROM_parameter_integrity ();
 
       // migration of the EEPROM values into the configuration data file format
-      memset( permanent_file, 0xff, EEPROM_FILE_SYSTEM_SIZE * sizeof( uint32_t));
-      bool result =
-	  permanent_data_file.set_memory_area(
-	      (uint32_t *)permanent_file,
-	      (uint32_t *)(permanent_file+EEPROM_FILE_SYSTEM_SIZE));
-      assert(result == true);
 
+      bool result;
       float value;
 
+      // use a temporary data file to fill in all found config parameters
+      uint32_t temp_file_storage[1024];
+      EEPROM_file_system temp_file;
+      temp_file.initialize_memory_area( temp_file_storage, temp_file_storage+1024);
+
       value = configuration (SENS_TILT_ROLL);
-      result = permanent_data_file.store_data (SENS_TILT_ROLL, 1, &value);
+      result = temp_file.store_data (SENS_TILT_ROLL, 1, &value);
 
       value = configuration (SENS_TILT_PITCH);
-      result = permanent_data_file.store_data (SENS_TILT_PITCH, 1, &value);
+      result = temp_file.store_data (SENS_TILT_PITCH, 1, &value);
 
       value = configuration (SENS_TILT_YAW);
-      result = permanent_data_file.store_data (SENS_TILT_YAW, 1, &value);
+      result = temp_file.store_data (SENS_TILT_YAW, 1, &value);
 
       value = configuration (PITOT_OFFSET);
-      result = permanent_data_file.store_data (PITOT_OFFSET, 1, &value);
+      result = temp_file.store_data (PITOT_OFFSET, 1, &value);
 
       value = configuration (PITOT_SPAN);
-      result = permanent_data_file.store_data (PITOT_SPAN, 1, &value);
+      result = temp_file.store_data (PITOT_SPAN, 1, &value);
       assert(result == true);
 
       value = configuration (QNH_OFFSET);
-      result = permanent_data_file.store_data (QNH_OFFSET, 1, &value);
+      result = temp_file.store_data (QNH_OFFSET, 1, &value);
       assert(result == true);
 
       value = configuration (VARIO_TC);
-      result = permanent_data_file.store_data (VARIO_TC, 1, &value);
+      result = temp_file.store_data (VARIO_TC, 1, &value);
       assert(result == true);
 
       value = configuration (VARIO_INT_TC);
-      result = permanent_data_file.store_data (VARIO_INT_TC, 1, &value);
+      result = temp_file.store_data (VARIO_INT_TC, 1, &value);
       assert(result == true);
 
       value = configuration (VARIO_P_TC);
-      result = permanent_data_file.store_data (VARIO_P_TC, 1, &value);
+      result = temp_file.store_data (VARIO_P_TC, 1, &value);
       assert(result == true);
 
       value = configuration (WIND_TC);
-      result = permanent_data_file.store_data (WIND_TC, 1, &value);
+      result = temp_file.store_data (WIND_TC, 1, &value);
       assert(result == true);
 
       value = configuration (MEAN_WIND_TC);
-      result = permanent_data_file.store_data (MEAN_WIND_TC, 1, &value);
+      result = temp_file.store_data (MEAN_WIND_TC, 1, &value);
       assert(result == true);
 
       value = configuration (HORIZON);
-      result = permanent_data_file.store_data (HORIZON, round (value));
+      result = temp_file.store_data (HORIZON, round (value));
       assert(result == true);
 
       value = configuration (GNSS_CONFIGURATION);
-      result = permanent_data_file.store_data (GNSS_CONFIGURATION,
+      result = temp_file.store_data (GNSS_CONFIGURATION,
 					       round (value));
       assert(result == true);
 
       value = configuration (ANT_BASELENGTH);
-      result = permanent_data_file.store_data (ANT_BASELENGTH, 1, &value);
+      result = temp_file.store_data (ANT_BASELENGTH, 1, &value);
       assert(result == true);
 
       value = configuration (ANT_SLAVE_DOWN);
-      result = permanent_data_file.store_data (ANT_SLAVE_DOWN, 1, &value);
+      result = temp_file.store_data (ANT_SLAVE_DOWN, 1, &value);
       assert(result == true);
 
       value = configuration (ANT_SLAVE_RIGHT);
-      result = permanent_data_file.store_data (ANT_SLAVE_RIGHT, 1, &value);
+      result = temp_file.store_data (ANT_SLAVE_RIGHT, 1, &value);
       assert(result == true);
 
-      permanent_data_file.dump_all_entries ();
+      temp_file.dump_all_entries ();
+
+      // now we initialize the permanent data file
+      permanent_data_file.initialize_memory_area(
+	      (uint32_t *)permanent_data_file_storage,
+	      (uint32_t *)(permanent_data_file_storage+EEPROM_FILE_SYSTEM_SIZE));
+
+      // and copy all collected information into it
+      permanent_data_file.import_all_data( temp_file);
+
+      // do some tests
       assert( permanent_data_file.is_consistent());
+      permanent_data_file.dump_all_entries ();
 
       char * slash = strrchr( path, '/');
       if( slash != 0)
 	slash[1] = 0;
       strcat( path, "configuration_data_file.dat");
-      ofstream perm_data_file (path, ios::out | ios::binary | ios::ate);
-      if (!perm_data_file.is_open ())
-	return false;
-
-      perm_data_file.write ((const char*) permanent_file,
-      EEPROM_FILE_SYSTEM_SIZE * sizeof(uint32_t));
-      perm_data_file.close ();
+      write_permanent_data_file( path);
       printf ("configuration_data_file.dat written - closing");
       exit (0);
     }
@@ -188,14 +193,6 @@ bool read_meta_data_file (char *file_path)
 void write_permanent_data_file( char * file_name)
 {
   assert( permanent_data_file.is_consistent() );
-
-  EEPROM_file_system_node copy_data_storage[EEPROM_FILE_SYSTEM_SIZE];
-  memset( copy_data_storage, 0xff, EEPROM_FILE_SYSTEM_SIZE * sizeof(uint32_t));
-  EEPROM_file_system permanent_data_copy( copy_data_storage, copy_data_storage+EEPROM_FILE_SYSTEM_SIZE);
-  bool result = permanent_data_copy.set_memory_area ( (uint32_t *)copy_data_storage, (uint32_t *)(copy_data_storage+EEPROM_FILE_SYSTEM_SIZE));
-  assert(result == true);
-  permanent_data_copy.import_all_data( permanent_data_file);
-
   char path[100];
   strcpy( path, file_name);
   char * slash = strrchr( path, '/');
@@ -209,6 +206,6 @@ void write_permanent_data_file( char * file_name)
       exit (0);
     }
 
-  perm_data_file_stream.write ((const char*) copy_data_storage, EEPROM_FILE_SYSTEM_SIZE * sizeof(uint32_t));
+  perm_data_file_stream.write ((const char*) permanent_data_file_storage, EEPROM_FILE_SYSTEM_SIZE * sizeof(uint32_t));
   perm_data_file_stream.close ();
 }
