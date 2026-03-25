301fe9ddc2837e2e7e869265cc62f2a3505333f6
realtimepeople <klaus.schaefer@h-da.de>
2026-03-25 13:41:43 +0100

    File conversion executed.

--- sensor_data_analyzer/Interfaces/persistent_data_file_emulation.cpp
+++ sensor_data_analyzer/Interfaces/persistent_data_file_emulation.cpp
@@ -30,167 +30,114 @@ bool read_meta_data_file (char *file_path)
       exit (-1);
     }
 
-  strcat (path, "configuration_data_file.dat");
-
-  // FIRST preference: read the configuration data file
-  ifstream config_file (path, ios::in | ios::binary | ios::ate);
-  if (config_file.is_open ())
-    {
-      streampos size = config_file.tellg ();
-      if (size != (EEPROM_FILE_SYSTEM_SIZE * sizeof(uint32_t)))
-	{
-	  printf ("configuration_data_file.dat : wrong file size\n");
-	  return -1;
-	}
-
-      config_file.seekg (0, ios::beg);
-      config_file.read ((char*) permanent_data_file_storage, size);
-      config_file.close ();
-
-      bool result = permanent_data_file.set_memory_to_existing_data( (uint32_t *)permanent_data_file_storage, (uint32_t *)(permanent_data_file_storage+EEPROM_FILE_SYSTEM_SIZE));
-      assert(result == true);
-
-      if ( not permanent_data_file.is_consistent ())
-	{
-	  printf ("configuration_data_file.dat : data not consistent, please delete it !\n");
-	  return -1;
-	}
-
-      printf ("\nConfig file read:\n");
-      permanent_data_file.dump_all_entries ();
-
-#if 1 // test data import
-      {
-	EEPROM_file_system_node copy_data_storage[EEPROM_FILE_SYSTEM_SIZE];
-	EEPROM_file_system <LOWEST_UNUSED_EEPROM_ID> permanent_data_copy( copy_data_storage, copy_data_storage+EEPROM_FILE_SYSTEM_SIZE);
-	permanent_data_copy.set_memory_virgin( (uint32_t *)copy_data_storage, (uint32_t *)(copy_data_storage+EEPROM_FILE_SYSTEM_SIZE));
-	assert(result == true);
-	permanent_data_copy.import_all_data( permanent_data_file);
-	permanent_data_copy.dump_all_entries ();
-      }
-#endif
-
-    }
+  slash = strrchr (path, '/');
+  if (slash != 0)
+    slash[1] = 0;
   else
     {
-      // SECOND preference: Read the meta-data in "config.EEPROM"
-      slash = strrchr (path, '/');
-      if (slash != 0)
-	slash[1] = 0;
-      else
-	{
-	  printf ("Unable to open configuration - exiting");
-	  exit (-1);
-	}
-      strcat (path, "config");
+      printf ("Unable to open configuration - exiting");
+      exit (-1);
+    }
+  strcat (path, "config");
+  if (read_EEPROM_file (path) != EXIT_SUCCESS)
+    {
+      // THIRD preference: Read the meta-data accompanying the *.f32 i.e. "*.EEPROM"
+      strcpy (path, file_path);
+      char *dot = strrchr (path, '.');
+      if (dot)
+	*dot = 0;
       if (read_EEPROM_file (path) != EXIT_SUCCESS)
 	{
-	  // THIRD preference: Read the meta-data accompanying the *.f32 i.e. "*.EEPROM"
-	  strcpy (path, file_path);
-	  char *dot = strrchr (path, '.');
-	  if (dot)
-	    *dot = 0;
-	  if (read_EEPROM_file (path) != EXIT_SUCCESS)
-	    {
-	      printf ("None of the configuration files found, exiting\n");
-	      return -1;
-	    }
+	  printf ("None of the configuration files found, exiting\n");
+	  return -1;
 	}
-
-      ensure_EEPROM_parameter_integrity ();
-
-      // migration of the EEPROM values into the configuration data file format
-
-      bool result;
-      float value;
-
-      // use a temporary data file to fill in all found config parameters
-      uint32_t temp_file_storage[1024];
-      EEPROM_file_system <LOWEST_UNUSED_EEPROM_ID> temp_file;
-      temp_file.initialize_memory_area( temp_file_storage, temp_file_storage+1024);
-
-      value = configuration (SENS_TILT_ROLL);
-      result = temp_file.store_data (SENS_TILT_ROLL, 1, &value);
-
-      value = configuration (SENS_TILT_PITCH);
-      result = temp_file.store_data (SENS_TILT_PITCH, 1, &value);
-
-      value = configuration (SENS_TILT_YAW);
-      result = temp_file.store_data (SENS_TILT_YAW, 1, &value);
-
-      value = configuration (PITOT_OFFSET);
-      result = temp_file.store_data (PITOT_OFFSET, 1, &value);
-
-      value = configuration (PITOT_SPAN);
-      result = temp_file.store_data (PITOT_SPAN, 1, &value);
-      assert(result == true);
-
-      value = configuration (QNH_OFFSET);
-      result = temp_file.store_data (QNH_OFFSET, 1, &value);
-      assert(result == true);
-
-      value = configuration (VARIO_TC);
-      result = temp_file.store_data (VARIO_TC, 1, &value);
-      assert(result == true);
-
-      value = configuration (VARIO_INT_TC);
-      result = temp_file.store_data (VARIO_INT_TC, 1, &value);
-      assert(result == true);
-
-      value = configuration (VARIO_P_TC);
-      result = temp_file.store_data (VARIO_P_TC, 1, &value);
-      assert(result == true);
-
-      value = configuration (WIND_TC);
-      result = temp_file.store_data (WIND_TC, 1, &value);
-      assert(result == true);
-
-      value = configuration (MEAN_WIND_TC);
-      result = temp_file.store_data (MEAN_WIND_TC, 1, &value);
-      assert(result == true);
-
-      value = configuration (HORIZON);
-      result = temp_file.store_data (HORIZON, 1, &value);
-      assert(result == true);
-
-      value = configuration (GNSS_CONFIGURATION);
-      result = temp_file.store_data (GNSS_CONFIGURATION, 1, &value);
-      assert(result == true);
-
-      value = configuration (ANT_BASELENGTH);
-      result = temp_file.store_data (ANT_BASELENGTH, 1, &value);
-      assert(result == true);
-
-      value = configuration (ANT_SLAVE_DOWN);
-      result = temp_file.store_data (ANT_SLAVE_DOWN, 1, &value);
-      assert(result == true);
-
-      value = configuration (ANT_SLAVE_RIGHT);
-      result = temp_file.store_data (ANT_SLAVE_RIGHT, 1, &value);
-      assert(result == true);
-
-      temp_file.dump_all_entries ();
-
-      // now we initialize the permanent data file
-      permanent_data_file.initialize_memory_area(
-	      (uint32_t *)permanent_data_file_storage,
-	      (uint32_t *)(permanent_data_file_storage+EEPROM_FILE_SYSTEM_SIZE));
-
-      // and copy all collected information into it
-      permanent_data_file.import_all_data( temp_file);
-
-      // do some tests
-      assert( permanent_data_file.is_consistent());
-      permanent_data_file.dump_all_entries ();
-
-      char * slash = strrchr( path, '/');
-      if( slash != 0)
-	slash[1] = 0;
-      strcat( path, "configuration_data_file.dat");
-      write_permanent_data_file( path);
-      printf ("configuration_data_file.dat written - closing");
-      exit (0);
     }
+
+  ensure_EEPROM_parameter_integrity ();
+
+  // migration of the EEPROM values into the configuration data file format
+
+  bool result;
+  float value;
+
+  // use a temporary data file to fill in all found config parameters
+  uint32_t temp_file_storage[1024];
+  EEPROM_file_system <LOWEST_UNUSED_EEPROM_ID> temp_file;
+  temp_file.initialize_memory_area( temp_file_storage, temp_file_storage+1024);
+
+  value = configuration (SENS_TILT_ROLL);
+  result = temp_file.store_data (SENS_TILT_ROLL, 1, &value);
+
+  value = configuration (SENS_TILT_PITCH);
+  result = temp_file.store_data (SENS_TILT_PITCH, 1, &value);
+
+  value = configuration (SENS_TILT_YAW);
+  result = temp_file.store_data (SENS_TILT_YAW, 1, &value);
+
+  value = configuration (PITOT_OFFSET);
+  result = temp_file.store_data (PITOT_OFFSET, 1, &value);
+
+  value = configuration (PITOT_SPAN);
+  result = temp_file.store_data (PITOT_SPAN, 1, &value);
+  assert(result == true);
+
+  value = configuration (QNH_OFFSET);
+  result = temp_file.store_data (QNH_OFFSET, 1, &value);
+  assert(result == true);
+
+  value = configuration (VARIO_TC);
+  result = temp_file.store_data (VARIO_TC, 1, &value);
+  assert(result == true);
+
+  value = configuration (VARIO_INT_TC);
+  result = temp_file.store_data (VARIO_INT_TC, 1, &value);
+  assert(result == true);
+
+  value = configuration (VARIO_P_TC);
+  result = temp_file.store_data (VARIO_P_TC, 1, &value);
+  assert(result == true);
+
+  value = configuration (WIND_TC);
+  result = temp_file.store_data (WIND_TC, 1, &value);
+  assert(result == true);
+
+  value = configuration (MEAN_WIND_TC);
+  result = temp_file.store_data (MEAN_WIND_TC, 1, &value);
+  assert(result == true);
+
+  value = configuration (HORIZON);
+  result = temp_file.store_data (HORIZON, 1, &value);
+  assert(result == true);
+
+  value = configuration (GNSS_CONFIGURATION);
+  result = temp_file.store_data (GNSS_CONFIGURATION, 1, &value);
+  assert(result == true);
+
+  value = configuration (ANT_BASELENGTH);
+  result = temp_file.store_data (ANT_BASELENGTH, 1, &value);
+  assert(result == true);
+
+  value = configuration (ANT_SLAVE_DOWN);
+  result = temp_file.store_data (ANT_SLAVE_DOWN, 1, &value);
+  assert(result == true);
+
+  value = configuration (ANT_SLAVE_RIGHT);
+  result = temp_file.store_data (ANT_SLAVE_RIGHT, 1, &value);
+  assert(result == true);
+
+  temp_file.dump_all_entries ();
+
+  // now we initialize the permanent data file
+  permanent_data_file.initialize_memory_area(
+	  (uint32_t *)permanent_data_file_storage,
+	  (uint32_t *)(permanent_data_file_storage+EEPROM_FILE_SYSTEM_SIZE));
+
+  // and copy all collected information into it
+  permanent_data_file.import_all_data( temp_file);
+
+  // do some tests
+  assert( permanent_data_file.is_consistent());
+  permanent_data_file.dump_all_entries ();
   return true;
 }
 
