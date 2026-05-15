4e4c12feae1fb94fcfd4ed543845456eca224946
realtimepeople <klaus.schaefer@h-da.de>
2026-05-15 11:29:53 +0200

    added *.f37 -> lrsx data importer

---
+++ convert_f37_to_lrsx/src/embedded_math.h
@@ -0,0 +1,66 @@
+/***********************************************************************//**
+ * @file    		embedded_math.h
+ * @brief   		defines platform-dependent algorithms and constants
+ * @author		Dr. Klaus Schaefer
+ * @copyright 		Copyright 2021 Dr. Klaus Schaefer. All rights reserved.
+ * @license 		This project is released under the GNU Public License GPL-3.0
+
+    <Larus Flight Sensor Firmware>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ **************************************************************************/
+
+#ifndef INC_EMBEDDED_MATH_H_
+#define INC_EMBEDDED_MATH_H_
+
+#ifndef _WIN32
+char* itoa( int value, char* result, int base=10);
+#endif
+
+#ifdef _MSC_VER
+# include "corecrt_math_defines.h"
+#endif
+#ifdef _WIN32
+# include <cmath>  // needed for inline abs
+#endif
+
+#include "math.h"
+#include "stdint.h"
+#include "float.h"
+#include "assert.h"
+
+#define ftype float
+typedef float float32_t;
+
+#define ZERO 0.0f
+#define ONE 1.0f
+#define TWO 2.0f
+#define HALF 0.5f
+#define QUARTER 0.25f
+#define M_PI_F M_PI
+
+#define SQR(x) ((x)*(x))
+#define SQRT(x) sqrtf(x)
+#define COS(x) cosf(x)
+#define SIN(x) sinf(x)
+#define ASIN(x) asinf(x)
+#define ATAN2(y, x) atan2f(y, x)
+
+template <typename type> type CLIP( type x, type min, type max)
+{
+  return x < min ? min : x > max ? max : x;
+}
+
+#endif /* INC_EMBEDDED_MATH_H_ */
---
+++ convert_f37_to_lrsx/src/system_configuration.h
@@ -0,0 +1,46 @@
+/***********************************************************************//**
+ * @file    		system_configuration.h
+ * @brief   		collection of system tuning parameters
+ * @author		Dr. Klaus Schaefer
+ * @copyright 		Copyright 2021 Dr. Klaus Schaefer. All rights reserved.
+ * @license 		This project is released under the GNU Public License GPL-3.0
+
+    <Larus Flight Sensor Firmware>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ **************************************************************************/
+
+#ifndef SRC_SYSTEM_CONFIGURATION_H_
+#define SRC_SYSTEM_CONFIGURATION_H_
+
+#define GIT_TAG_DEC 0x12345678 // dummy
+
+#define UNIX 1   // Indicates to the lib that running on Unix/Windows but not on stm32
+
+#define TCP_PORT 8880	// XCsoar wants this one
+
+#define INCLUDING_NANO	 		1
+
+#define DEVELOPMENT_ADDITIONS		1
+#define EEPROM_WRITES_LOGGED		1
+#define DISABLE_SAT_COMPASS		0
+#define REPORT_MAGNETIC_CALIBRATION	1
+
+#define WITH_EXTERNAL_MAGNETOMETER	0
+#define SIMULATE_EXTERNAL_MAGNETOMETER	0
+
+#define PRINT_3D_MAG_PARAMETERS		1
+
+#endif /* SRC_SYSTEM_CONFIGURATION_H_ */
